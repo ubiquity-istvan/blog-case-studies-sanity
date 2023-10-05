5ecb5c4c5ad0639336d15f62e4b38ba836b7c9fc
istvantothdesign <istvan@ubiquitygroup.co.uk>
2023-10-05 10:57:35 +0100

    naxt cache original

--- next.config.js
+++ next.config.js
@@ -1,19 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/about",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "s-maxage=1, stale-while-revalidate=59",
-          },
-        ],
-      },
-    ];
-  },
   // images: {
   //   domains: ["cdn.sanity.io"],
   // },
